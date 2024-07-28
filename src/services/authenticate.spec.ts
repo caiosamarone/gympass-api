91e50f11d0b05fadf3292ae0b1fe73656303fb23
Caio Mendes <caiosamarone10@hotmail.com>
2024-07-28 20:11:37 -0300

    refactor unit tests

--- src/services/authenticate.spec.ts
+++ src/services/authenticate.spec.ts
@@ -1,14 +1,19 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 
 import { hash } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AuthenticateService } from './authenticate'
 import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
+let usersRepository: InMemoryUsersRepository
+let sut: AuthenticateService
+
 describe('authenticate service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateService(usersRepository)
+  })
   it('should authenticate user', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateService(usersRepository)
     const userMock = {
       name: 'John Doe',
       email: 'john.does@mail.com',
@@ -22,9 +27,6 @@ describe('authenticate service', () => {
     expect(user.id).toEqual(expect.any(String))
   })
   it('should not authenticate with wrong email', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateService(usersRepository)
-
     expect(async () => {
       await sut.execute({
         email: 'john.does@mail.com',
@@ -34,8 +36,6 @@ describe('authenticate service', () => {
   })
 
   it('should not authenticate with wrong password', async () => {
-    const usersRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateService(usersRepository)
     const userMock = {
       name: 'John Doe',
       email: 'john.does@mail.com',
--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,31 +1,34 @@
-import { expect, test, describe, it } from 'vitest'
+import { expect, test, describe, it, beforeEach } from 'vitest'
 import { RegisterService } from './register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { compare, hash } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterService
+
 describe('register service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterService(usersRepository)
+  })
   it('should create user', async () => {
-    const usersRepositoryInMemory = new InMemoryUsersRepository()
-    const registerService = new RegisterService(usersRepositoryInMemory)
     const userMock = {
       name: 'John Doe',
       email: 'john.does@mail.com',
       password: '123455687',
     }
-    const { user } = await registerService.execute(userMock)
+    const { user } = await sut.execute(userMock)
     expect(user.id).toEqual(expect.any(String))
   })
   it('should hash user password upon registration', async () => {
-    const usersRepositoryInMemory = new InMemoryUsersRepository()
-    const registerService = new RegisterService(usersRepositoryInMemory)
     const userMock = {
       name: 'John Doe',
       email: 'john.does@mail.com',
       password: '123455687',
     }
-    const { user } = await registerService.execute(userMock)
+    const { user } = await sut.execute(userMock)
     const isPasswordCorrectlyHashed = await compare(
       userMock.password,
       user.passoword_hash,
@@ -34,8 +37,6 @@ describe('register service', () => {
   })
 
   it('should not register duplicate emails', async () => {
-    const usersRepositoryInMemory = new InMemoryUsersRepository()
-    const registerService = new RegisterService(usersRepositoryInMemory)
     const firstUserMock = {
       name: 'Rafael Leao',
       email: 'john.does@mail.com',
@@ -46,9 +47,9 @@ describe('register service', () => {
       email: 'john.does@mail.com',
       password: '123455687',
     }
-    await registerService.execute(firstUserMock)
-    await expect(() =>
-      registerService.execute(secondUserMock),
-    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
+    await sut.execute(firstUserMock)
+    await expect(() => sut.execute(secondUserMock)).rejects.toBeInstanceOf(
+      UserAlreadyExistsError,
+    )
   })
 })
